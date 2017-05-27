dfff2ae6bbf852cada30346a1192e6f1a38d1d2b
stamo.petkov@gmail.com <stamo.petkov@gmail.com>
2017-05-27 10:20:01 +0300

    Books migrations

---
+++ NbuBookLibrary/NbuBookLibrary/BooksMigrations/201705270718376_initBooks.cs
@@ -0,0 +1,43 @@
+namespace NbuBookLibrary.BooksMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initBooks : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        AuthorId = c.Int(nullable: false),
+                        Description = c.String(maxLength: 500),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
