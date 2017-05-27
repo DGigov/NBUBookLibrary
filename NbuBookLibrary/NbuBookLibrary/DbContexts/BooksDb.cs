9dc1b14487ee3f8c8505f578fd009cc20d3f5def
stamo.petkov@gmail.com <stamo.petkov@gmail.com>
2017-05-27 10:14:15 +0300

    BookDb added

---
+++ NbuBookLibrary/NbuBookLibrary/DbContexts/BooksDb.cs
@@ -0,0 +1,18 @@
+﻿using NbuBookLibrary.Models.BooksModels;
+using System.Data.Entity;
+
+namespace NbuBookLibrary.DbContexts
+{
+    public class BooksDb : DbContext
+    {
+        public BooksDb()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ NbuBookLibrary/NbuBookLibrary/Models/BooksModels/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NbuBookLibrary.Models.BooksModels
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        [ForeignKey(nameof(AuthorId))]
+        public virtual Author Author { get; set; }
+
+    }
+}
